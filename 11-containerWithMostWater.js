b7868c965e77b0e0198bf3183938b03d3ad9250d
Kevin Nielsen <knielsen24@gmail.com>
2023-01-26 14:40:17 -0800

    optimized 11

--- 11-containerWithMostWater.js
+++ 11-containerWithMostWater.js
@@ -6,29 +6,26 @@
  * In this case, the max area of water (blue section) the container can contain is 49.} height
  */
 
-var maxArea = function(height) {
-    let left = 0
-    let right = height.length - 1
-    let maxContainer = 0
+var maxArea = function (height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxContainer = 0;
 
-    const container = (num1, num2, arr) => {
-        let short = (arr[num1] <= arr[num2]) ? arr[num1] : arr[num2]
-        return short * (num2 - num1)
-    }
+    // const container = (num1, num2, arr) => {
+    //     return Math.min(arr[num1], arr[num2]) * (num2 - num1);
+    // };
 
     while (left !== right) {
-        let temp = container(left, right, height)
+        let temp = Math.min(height[left], height[right]) * (right - left)
+        // let temp = container(left, right, height);
         if (temp > maxContainer) {
-            maxContainer = temp
-        } else if(height[left] < height[right]){
-            left++
-        } else {
-            right--
+            maxContainer = temp;
         }
+        height[left] < height[right] ? left++ : right--;
     }
-    return maxContainer
+    return maxContainer;
 };
 
-const height = [1,1]
+const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 
-console.log(maxArea(height))
+console.log(maxArea(height));
