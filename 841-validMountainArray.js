5fa34b6be5b9f9812f54ba6c9684ef688bd10be0
Kevin Nielsen <knielsen24@gmail.com>
2023-02-13 14:42:15 -0800

    solved 841 and added explanation

--- 841-validMountainArray.js
+++ 841-validMountainArray.js
@@ -1,27 +1,37 @@
 var validMountainArray = function (arr) {
     if (arr.length < 3) return false;
-    let mountain = true;
-    let peak = arr.indexOf(Math.max(...arr));
+    // length of arr must be 3 or more
 
-    if(peak === 0 || peak === arr.length -1) return false
+    let mountain = true;
+    // mountain will be the return if conditions down below do not return false
+    let peak = arr.indexOf(Math.max(...arr));
+    // finding the peak (max) of the arry
+
+    if (peak === 0 || peak === arr.length - 1) return false;
+    // returns false if the peak is at index 0 or last index
 
     let left = peak - 1;
     let right = peak;
+    // this assigns left and right some where in "middle" of the array
+    // right and left should be stricly decreasing from the peak
 
     while (left > 0) {
-        if(arr[left -1] >= arr[left]) return false
-
+        // looking at the peak to index 0
+        if (arr[left - 1] >= arr[left]) return false;
+        // returns false if not decreasing in value
         left--;
+        // if the condition above is not triggered left moves towards index 0
     }
 
     while (right < arr.length) {
-        if(arr[right] <= arr[right+1]) return false
+        // looking at the peak to last index
+        if (arr[right] <= arr[right + 1]) return false;
+        // returns false if not decreasing in value
         right++;
+        // if the condition above is not triggered right moves towards last index
     }
 
-    return mountain
+    return mountain;
 };
 
-console.log(validMountainArray([1, 2, 1]));
-
-
+console.log(validMountainArray([1, 2, 3, 1]));
