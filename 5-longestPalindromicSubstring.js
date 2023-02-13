4dc27c1239cd204fe37ea9aa32884b918d757823
Kevin Nielsen <knielsen24@gmail.com>
2023-02-13 11:35:01 -0800

    added file

---
+++ 5-longestPalindromicSubstring.js
@@ -0,0 +1,7 @@
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+var longestPalindrome = function(s) {
+
+};
--- 852-peakIndexInAMountainArray.js
+++ 852-peakIndexInAMountainArray.js
@@ -1,29 +1,26 @@
-
 /**
  * @param {number[]} arr
  * @return {number}
  */
-var peakIndexInMountainArray = function(arr) {
-    const originalLen = arr.length
-    let left = 0
-    let right = originalLen - 1
-    let mid = Math.floor((left + right)/2)
+var peakIndexInMountainArray = function (arr) {
+    const originalLen = arr.length;
+    let left = 0;
+    let right = originalLen - 1;
+    let mid = Math.floor((left + right) / 2);
 
     while (left < mid && mid < originalLen) {
-      if ( arr[mid-1] < arr[mid] && arr[mid] < arr[mid+1] ) {
-        left = mid
-      }
-      else if (arr[mid-1] > arr[mid] && arr[mid] > arr[mid+1] ) {
-        right = mid
-      }
-      else if (arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1] ) return mid;
+        if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) {
+            left = mid;
+        } else if (arr[mid - 1] > arr[mid] && arr[mid] > arr[mid + 1]) {
+            right = mid;
+        } else if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
 
-      mid = Math.floor((left + right)/2)
+        mid = Math.floor((left + right) / 2);
     }
-
 };
 
-const arr1 = [0,10,5,2]
-const arr2 = [0,10,15,20, 14]
+const arr1 = [0, 10, 5, 2];
+const arr2 = [0, 10, 15, 20, 14];
 
-console.log(peakIndexInMountainArray(arr2))
+console.log(peakIndexInMountainArray(arr2));
